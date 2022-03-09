b5964b056d3b3bb6f67edb03d74256113472e77a
David <56879924+dav1dexe@users.noreply.github.com>
2022-03-09 15:30:42 +0400

    reDesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,22 @@
-let media = window.matchMedia("(max-width: 500px)");
-
-let advantageItem = document.getElementsByClassName("advantage__item");
+let media = window.matchMedia("(max-width: 850px)");
+let feauturesItem = document.getElementsByClassName("feautures__item");
 
 function mediaQuery() {
-    if (media.matches) {
 
-        for (let i = 0; i < advantageItem.length; i++) {
-            let child = advantageItem[i].childNodes;
+    if (media.matches) {
+        for (let i = 0; i < feauturesItem.length; i++) {
+            let child = feauturesItem[i].childNodes;
 
             if (child[1].nodeName == "IMG") {
                 child[0].after(child[3]);
             }
         }
     } else {
-        for (let i = 0; i < advantageItem.length; i++) {
-            let child = advantageItem[i].childNodes;
+        for (let i = 0; i < feauturesItem.length; i++) {
+            let child = feauturesItem[i].childNodes;
 
             if (child[1].nodeName == "DIV") {
-                if (i == 1 || i == 3 || i == 5) {
+                if (i == 1 || i == 3 || i == 5 || i == 7) {
                     child[3].after(child[1]);
                 }
             }
